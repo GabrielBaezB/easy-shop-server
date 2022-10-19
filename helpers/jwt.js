e8486ccfd8b2f77a22cc4ca690eab97ae31b57d3
Gabriel Ignacio Baez <45924381+GabrielBaezB@users.noreply.github.com>
2022-10-19 13:50:36 -0300

    Update jwt.js

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -13,7 +13,8 @@ function authJwt() {
             {url: /\/api\/v1\/products(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/categories(.*)/ , methods: ['GET', 'OPTIONS'] },
             {url: /\/api\/v1\/orders(.*)/,methods: ['GET', 'OPTIONS', 'POST']},
-            { url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS'] },
+            {url: /\/api\/v1\/users(.*)/, methods: ['GET', 'OPTIONS']},
+            {url: /\/api\/v1\/users\/.*/,métodos: ['GET']},
             `${api}/users/login`,
             `${api}/users/register`,
         ]
